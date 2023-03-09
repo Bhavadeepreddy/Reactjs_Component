a412e0d85e14611e6fb606d9395bc39304484ca4
Bhavadeepreddy <84385855+Bhavadeepreddy@users.noreply.github.com>
2023-03-09 16:32:13 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Heading from "./Heading.jsx";
+import Lists from "./Lists.jsx";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Lists />
+    </div>
+  );
+}
+export default App;
---
+++ Lists.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Lists() {
+  return (
+    <ul>
+      <li>Biryani</li>
+      <li>Jamoon</li>
+      <li>Sweets</li>
+    </ul>
+  );
+}
+
+export default Lists;
